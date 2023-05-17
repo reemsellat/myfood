18b6d01de42d8181e207222a264dd1120eba7131
reemsellat <reemsellat@gmail.com>
2023-05-17 20:13:24 +0300

    first commit

--- src/About.tsx
+++ src/About.tsx
@@ -4,7 +4,7 @@ function About() {
 
        return (
               <div id="about">
-                     <img src='/images/about.jpg'></img>
+                     <img src='images/about.jpg'></img>
                      <p>
                             In that ethos, we’ve opened My Food locations all across New York and Los Angeles-–with more to come.
                             We are known for our chicken and waffles, we add new versions all the time, but our menu also goes way beyond that! Other popular dishes include our signature buttermilk biscuit breakfast sandwiches, crispy chicken sandwiches, fresh market salads, and our signature maple lemonades.
